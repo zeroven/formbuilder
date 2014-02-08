a2348b5f827ab95cd5fb8086c1071c70a5aba670
wenwen.caoww@alibaba-inc.com <wenwen.caoww@alibaba-inc.com>
2014-02-08 11:21:36 +0800

    rename file

---
+++ dist/module.js
@@ -0,0 +1,54 @@
+define(function(require, exports, module){
+	//
+	console.info(111);
+	var Formbuilder = require('./formbuilder');
+	console.info(234);
+    $(function(){
+      fb = new Formbuilder({
+        selector: '.fb-main',
+        bootstrapData: [
+          {
+            "label": "Do you have a website?",
+            "field_type": "website",
+            "required": false,
+            "field_options": {},
+            "cid": "c1"
+          },
+          {
+            "label": "Please enter your clearance number",
+            "field_type": "text",
+            "required": true,
+            "field_options": {},
+            "cid": "c6"
+          },
+          {
+            "label": "Security personnel #82?",
+            "field_type": "radio",
+            "required": true,
+            "field_options": {
+                "options": [{
+                    "label": "Yes",
+                    "checked": false
+                }, {
+                    "label": "No",
+                    "checked": false
+                }],
+                "include_other_option": true
+            },
+            "cid": "c10"
+          },
+          {
+            "label": "Medical history",
+            "field_type": "file",
+            "required": true,
+            "field_options": {},
+            "cid": "c14"
+          }
+        ]
+      });
+
+      fb.on('save', function(payload){
+        //console.log(payload);
+      })
+    });
+})
