11c9e748e1e40d31fe380133b8705ae39f31a15b
wenwen.caoww@alibaba-inc.com <wenwen.caoww@alibaba-inc.com>
2014-02-08 16:41:02 +0800

    add jq ui

--- dist/module.js
+++ dist/module.js
@@ -4,6 +4,7 @@ define(function(require, exports, module){
     console.info(111,$);
     var $ = jQuery;
     console.info(123,$);
+    
     require('./formbuilder');
     console.info(234);
     return;
