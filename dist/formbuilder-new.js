7f869d8831cfa4a99a74f3228e19f447e49bfa00
wenwen.caoww@alibaba-inc.com <wenwen.caoww@alibaba-inc.com>
2014-02-11 11:23:30 +0800

    mod

--- dist/formbuilder-new.js
+++ dist/formbuilder-new.js
@@ -6,11 +6,13 @@ define(function(require, exports, module){
     // require('underscore');
     // require('rivets');
     // require('backbone');
-    require('jqueryui'); 
+    console.info(1,$)
     var _ = require('underscore');
+    console.info(2,_)
     var rivets = require('rivets'); 
+    console.info(3,rivets)
     var Backbone = require('backbone');
-    console.info(Backbone,_,$,rivets)
+    console.info(4,Backbone)
 
     var _root = window;
 
