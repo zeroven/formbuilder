4c47a88c3e0e7d1cc7d92ed58af2d263a86d1e25
wenwen.caoww@alibaba-inc.com <wenwen.caoww@alibaba-inc.com>
2014-02-11 17:06:01 +0800

    back

--- dist/formbuilder-new.js
+++ dist/formbuilder-new.js
@@ -65,9 +65,6 @@ define(function(require, exports, module){
 
   var BuilderView, EditFieldView, Formbuilder, FormbuilderCollection, FormbuilderModel, ViewFieldView, _ref, _ref1, _ref2, _ref3, _ref4,
     __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-
-    /**
     __extends = function (child, parent) {
       for (var key in parent) {
         if (__hasProp.call(parent, key))
@@ -95,7 +92,8 @@ define(function(require, exports, module){
       }
       return child;
     };
-    */
+    //__extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
 
   FormbuilderModel = (function(_super) {
     __extends(FormbuilderModel, _super);
